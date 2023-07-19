67d3174697120fdbb437a7779de4a9c8cfec91b2
Lans12345 <olanalans12345@gmail.com>
2023-07-19 17:49:34 +0800

    added initial upload pic screen

--- lib/screens/add_image_screen.dart
+++ lib/screens/add_image_screen.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
 
+import '../utils/colors.dart';
+
 class AddImageScreen extends StatelessWidget {
   const AddImageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: primary,
+        ),
+        child: SafeArea(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Center(
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Icon(
+                    Icons.file_open_outlined,
+                    color: Colors.grey,
+                    size: 180,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
   }
 }
