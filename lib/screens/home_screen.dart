a8d174fb5b0130e771e89738bfc461aaf1ad81ed
Lans12345 <olanalans12345@gmail.com>
2023-07-19 15:44:50 +0800

    added initial home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:fotoverse/utils/colors.dart';
+import 'package:fotoverse/widgets/text_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: red,
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          onPressed: () {}),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: primary,
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    TextBold(
+                      text: 'Welcome John,',
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                    const Expanded(
+                      child: SizedBox(
+                        width: 10,
+                      ),
+                    ),
+                    const CircleAvatar(
+                      minRadius: 20,
+                      maxRadius: 20,
+                      backgroundImage: AssetImage(
+                        'assets/images/profile.png',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: TextBold(
+                    text: 'Public Page',
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: SizedBox(
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2),
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Container(
+                            height: 350,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
