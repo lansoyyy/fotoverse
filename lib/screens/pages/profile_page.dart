d892848443d016e6e4eb7f690ba00bac331c5248
Lans12345 <olanalans12345@gmail.com>
2023-07-26 13:27:31 +0800

    fetched data to profile

--- lib/screens/pages/profile_page.dart
+++ lib/screens/pages/profile_page.dart
@@ -1,10 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fotoverse/screens/auth/login_screen.dart';
 import 'package:fotoverse/widgets/button_widget.dart';
 import 'package:fotoverse/widgets/text_widget.dart';
 import 'package:fotoverse/widgets/textfield_widget.dart';
 
 import '../../utils/colors.dart';
 import '../../widgets/drawer_widget.dart';
+import '../../widgets/toast_widget.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -20,6 +24,10 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
+    final Stream<DocumentSnapshot> userData = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .snapshots();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: primary,
@@ -32,91 +40,155 @@ class _ProfilePageState extends State<ProfilePage> {
         ],
       ),
       drawer: const DrawerWidget(),
-      body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            color: primary,
-          ),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 20,
+      body: StreamBuilder<DocumentSnapshot>(
+          stream: userData,
+          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return const Drawer();
+            } else if (snapshot.hasError) {
+              return const Center(child: Text('Something went wrong'));
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Drawer();
+            }
+            dynamic data = snapshot.data;
+            return Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: primary,
                 ),
-                const CircleAvatar(
-                  minRadius: 50,
-                  maxRadius: 50,
-                  backgroundImage: AssetImage(
-                    'assets/images/profile.png',
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextBold(
-                  text: 'John Doe',
-                  fontSize: 32,
-                  color: Colors.white,
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 50),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 20,
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
-                        child: TextBold(
-                          text: 'M',
-                          fontSize: 18,
-                          color: primary,
+                      const CircleAvatar(
+                        minRadius: 50,
+                        maxRadius: 50,
+                        backgroundImage: AssetImage(
+                          'assets/images/profile.png',
                         ),
                       ),
-                    ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextBold(
+                        text: data['name'],
+                        fontSize: 32,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 50),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                              child: TextBold(
+                                text: data['gender'],
+                                fontSize: 18,
+                                color: primary,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFieldWidget(
+                        isRequred: false,
+                        label: 'Email',
+                        hint: data['email'],
+                        controller: emailController,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFieldWidget(
+                        hint: '********',
+                        isObscure: true,
+                        label: 'Password',
+                        controller: passwordController,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ButtonWidget(
+                        color: red,
+                        labelColor: Colors.white,
+                        label: 'Delete Account',
+                        onPressed: () {
+                          final user = FirebaseAuth.instance.currentUser;
+                          showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                    title: const SizedBox(
+                                      width: 150,
+                                      child: Text(
+                                        'Are you sure you want to delete your account?',
+                                        style: TextStyle(
+                                            color: Colors.red,
+                                            fontFamily: 'QBold',
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    content: const Text(
+                                      'This action cannot be undone',
+                                      style: TextStyle(fontFamily: 'QRegular'),
+                                    ),
+                                    actions: <Widget>[
+                                      MaterialButton(
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(true),
+                                        child: const Text(
+                                          'Close',
+                                          style: TextStyle(
+                                              fontFamily: 'QRegular',
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                      MaterialButton(
+                                        onPressed: () async {
+                                          await FirebaseAuth.instance
+                                              .signOut()
+                                              .then((value) {
+                                            user!.delete().then((value) {
+                                              Navigator.of(context).pushReplacement(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const LoginScreen()));
+                                            });
+                                          });
+                                          showToast(
+                                              'Account deleted succesfully!');
+                                        },
+                                        child: const Text(
+                                          'Continue',
+                                          style: TextStyle(
+                                              fontFamily: 'QRegular',
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.red),
+                                        ),
+                                      ),
+                                    ],
+                                  ));
+                        },
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFieldWidget(
-                  label: 'Name',
-                  controller: nameController,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFieldWidget(
-                  label: 'Email',
-                  controller: emailController,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFieldWidget(
-                  isObscure: true,
-                  label: 'Password',
-                  controller: passwordController,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ButtonWidget(
-                  color: red,
-                  labelColor: Colors.white,
-                  label: 'Delete Account',
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          )),
+                ));
+          }),
     );
   }
 }
--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fotoverse/screens/auth/login_screen.dart';
 import 'package:fotoverse/screens/home_screen.dart';
@@ -19,128 +21,146 @@ class _MyDrawerState extends State<DrawerWidget> {
   //     FirebaseFirestore.instance.collection('Users').doc(userId).snapshots();
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 220,
-      child: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.only(top: 0),
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              decoration: const BoxDecoration(
-                color: primary,
-              ),
-              accountEmail:
-                  TextRegular(text: 'Email', fontSize: 12, color: Colors.white),
-              accountName: TextBold(
-                text: 'Name',
-                fontSize: 14,
-                color: Colors.white,
-              ),
-              currentAccountPicture: const Padding(
-                padding: EdgeInsets.all(5.0),
-                child: CircleAvatar(
-                  minRadius: 50,
-                  maxRadius: 50,
-                  backgroundImage: AssetImage('assets/images/profile.png'),
-                ),
+    final Stream<DocumentSnapshot> userData = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .snapshots();
+    return StreamBuilder<DocumentSnapshot>(
+        stream: userData,
+        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const Drawer();
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Drawer();
+          }
+          dynamic data = snapshot.data;
+          return SizedBox(
+            width: 220,
+            child: Drawer(
+              child: ListView(
+                padding: const EdgeInsets.only(top: 0),
+                children: <Widget>[
+                  UserAccountsDrawerHeader(
+                    decoration: const BoxDecoration(
+                      color: primary,
+                    ),
+                    accountEmail: TextRegular(
+                        text: data['email'], fontSize: 12, color: Colors.white),
+                    accountName: TextBold(
+                      text: data['name'],
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                    currentAccountPicture: const Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: CircleAvatar(
+                        minRadius: 50,
+                        maxRadius: 50,
+                        backgroundImage:
+                            AssetImage('assets/images/profile.png'),
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: TextBold(
+                      text: 'Home',
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                    },
+                  ),
+                  ListTile(
+                    title: TextBold(
+                      text: 'My Profile',
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const ProfilePage()));
+                    },
+                  ),
+                  ListTile(
+                    title: TextBold(
+                      text: 'My Creations',
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const MyCreationsPage()));
+                    },
+                  ),
+                  ListTile(
+                    title: TextBold(
+                      text: 'About Fotoverse',
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const AboutUsPage()));
+                    },
+                  ),
+                  ListTile(
+                    title: TextBold(
+                      text: 'Logout',
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                                title: const Text(
+                                  'Logout Confirmation',
+                                  style: TextStyle(
+                                      fontFamily: 'QBold',
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                content: const Text(
+                                  'Are you sure you want to Logout?',
+                                  style: TextStyle(fontFamily: 'QRegular'),
+                                ),
+                                actions: <Widget>[
+                                  MaterialButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(true),
+                                    child: const Text(
+                                      'Close',
+                                      style: TextStyle(
+                                          fontFamily: 'QRegular',
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  MaterialButton(
+                                    onPressed: () async {
+                                      await FirebaseAuth.instance.signOut();
+                                      Navigator.of(context).pushReplacement(
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const LoginScreen()));
+                                    },
+                                    child: const Text(
+                                      'Continue',
+                                      style: TextStyle(
+                                          fontFamily: 'QRegular',
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              ));
+                    },
+                  ),
+                ],
               ),
             ),
-            ListTile(
-              title: TextBold(
-                text: 'Home',
-                fontSize: 12,
-                color: Colors.black,
-              ),
-              onTap: () {
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => const HomeScreen()));
-              },
-            ),
-            ListTile(
-              title: TextBold(
-                text: 'My Profile',
-                fontSize: 12,
-                color: Colors.black,
-              ),
-              onTap: () {
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => const ProfilePage()));
-              },
-            ),
-            ListTile(
-              title: TextBold(
-                text: 'My Creations',
-                fontSize: 12,
-                color: Colors.black,
-              ),
-              onTap: () {
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => const MyCreationsPage()));
-              },
-            ),
-            ListTile(
-              title: TextBold(
-                text: 'About Fotoverse',
-                fontSize: 12,
-                color: Colors.black,
-              ),
-              onTap: () {
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => const AboutUsPage()));
-              },
-            ),
-            ListTile(
-              title: TextBold(
-                text: 'Logout',
-                fontSize: 12,
-                color: Colors.black,
-              ),
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                          title: const Text(
-                            'Logout Confirmation',
-                            style: TextStyle(
-                                fontFamily: 'QBold',
-                                fontWeight: FontWeight.bold),
-                          ),
-                          content: const Text(
-                            'Are you sure you want to Logout?',
-                            style: TextStyle(fontFamily: 'QRegular'),
-                          ),
-                          actions: <Widget>[
-                            MaterialButton(
-                              onPressed: () => Navigator.of(context).pop(true),
-                              child: const Text(
-                                'Close',
-                                style: TextStyle(
-                                    fontFamily: 'QRegular',
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            MaterialButton(
-                              onPressed: () async {
-                                // await FirebaseAuth.instance.signOut();
-                                Navigator.of(context).pushReplacement(
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const LoginScreen()));
-                              },
-                              child: const Text(
-                                'Continue',
-                                style: TextStyle(
-                                    fontFamily: 'QRegular',
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ],
-                        ));
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
