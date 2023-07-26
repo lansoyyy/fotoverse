d323dc4459b6fa693629908eec7dbdb3c95665cb
Lans12345 <olanalans12345@gmail.com>
2023-07-26 13:40:51 +0800

    added auto login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:fotoverse/screens/auth/get_started_screen.dart';
 import 'package:fotoverse/screens/auth/landing_screen.dart';
 import 'package:fotoverse/screens/home_screen.dart';
 
